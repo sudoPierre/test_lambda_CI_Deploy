100286edf5b04d9d5f85d66d98aba4c419543fbf
sudoPierre <pierre.galliot.pg@outlook.fr>
2025-11-24 20:36:31 +0100

    Fix print statement to use consistent naming for event key

--- lambda/hello-python.py
+++ lambda/hello-python.py
@@ -6,7 +6,7 @@ print('Loading function')
 def lambda_handler(event, context):
     #print("Received event: " + json.dumps(event, indent=2))
     print("valueTest = " + event['key1'])
-    print("value2 = " + event['key2'])
+    print("valueTest2 = " + event['key2'])
     print("value3 = " + event['key3'])
     return event['key1']  # Echo back the first key value
     #raise Exception('Something went wrong')
